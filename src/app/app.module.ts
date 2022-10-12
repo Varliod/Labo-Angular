60c5851ce30179f9111f46473eb9b033e95a27ed
Varliod <david.gilmard@gmail.com>
2022-10-12 17:11:46 +0200

    rooting bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './shared/home/home.component';
-import { MachinComponent } from './machin/machin.component';
-import { ChouetteComponent } from './chouette/chouette.component';
-import { ChoseComponent } from './chose/chose.component';
-import { BiduleComponent } from './bidule/bidule.component';
-import { BazardComponent } from './bazard/bazard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    MachinComponent,
-    ChouetteComponent,
-    ChoseComponent,
-    BiduleComponent,
-    BazardComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
